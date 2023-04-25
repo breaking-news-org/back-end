4cc669d6a43f76f4d8ad0ef2ff4ec6f6fda3ae5d
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-04-25 20:03:31 +0300

    upd api

--- back/src/API/Endpoints/API1/News.hs
+++ back/src/API/Endpoints/API1/News.hs
@@ -1,6 +1,6 @@
 module API.Endpoints.API1.News where
 
-import API.Prelude (Generic, GenericMode (type (:-)), JSON, NoContent, Post, Put, ReqBody, type (:>))
+import API.Prelude (Generic, GenericMode (type (:-)), JSON, NoContent, Post, ReqBody, type (:>), Get)
 import API.Types.News (CreateNews, QueryParams)
 import Servant.Record (RecordParam)
 import Service.Types.News qualified as API (GetNews)
@@ -8,6 +8,6 @@ import API.Types.Instances (Drop)
 
 data API route = API
   { create :: route :- "create" :> ReqBody '[JSON] CreateNews :> Post '[JSON] NoContent
-  , get :: route :- "get" :> RecordParam Drop QueryParams :> Put '[JSON] [API.GetNews]
+  , get :: route :- "get" :> RecordParam Drop QueryParams :> Get '[JSON] [API.GetNews]
   }
   deriving (Generic)
